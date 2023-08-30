7cfcc599d578ab8d39094614c61959b190e9006a
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-08-30 15:17:10 +0900

    firstCommit

---
+++ Source/MidNightProject/MidNightProjectCharacter.cpp
@@ -0,0 +1,134 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MidNightProjectCharacter.h"
+#include "MidNightProjectProjectile.h"
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "instrument.h"
+
+
+//////////////////////////////////////////////////////////////////////////
+// AMidNightProjectCharacter
+
+AMidNightProjectCharacter::AMidNightProjectCharacter()
+{
+	// Character doesnt have a rifle at start
+	bHasRifle = false;
+	
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+		
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f)); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	//Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
+	Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
+
+}
+
+void AMidNightProjectCharacter::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+
+}
+
+void AMidNightProjectCharacter::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	FHitResult HitResult;
+	FVector StartV=FirstPersonCameraComponent->GetComponentLocation();
+	FVector EndV = StartV+(FirstPersonCameraComponent->GetForwardVector()*1000);
+
+	if (GetWorld()->LineTraceSingleByChannel(HitResult,StartV,EndV,ECollisionChannel::ECC_Visibility))
+	{
+		AActor* HitActor=HitResult.GetActor();
+		Ainstrument* Hitinstrument=Cast<Ainstrument>(HitActor);
+		if (Hitinstrument!=nullptr)
+		{
+			UE_LOG(LogTemp,Warning,TEXT("Id=%d"),Hitinstrument->id);
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *Hitinstrument->infoString);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("InstrumentFalse"));
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////// Input
+
+void AMidNightProjectCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		//Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+		//Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMidNightProjectCharacter::Move);
+
+		//Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMidNightProjectCharacter::Look);
+	}
+}
+
+
+void AMidNightProjectCharacter::Move(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add movement 
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void AMidNightProjectCharacter::Look(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void AMidNightProjectCharacter::SetHasRifle(bool bNewHasRifle)
+{
+	bHasRifle = bNewHasRifle;
+}
+
+bool AMidNightProjectCharacter::GetHasRifle()
+{
+	return bHasRifle;
+}
