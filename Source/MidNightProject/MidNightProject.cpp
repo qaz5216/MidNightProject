ac3cfa8e7585d73440a45d4356bab439593f4df4
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-09-07 10:26:37 +0900

    mergeerror

--- Source/MidNightProject/MidNightProject.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MidNightProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MidNightProject, "MidNightProject" );
- 
